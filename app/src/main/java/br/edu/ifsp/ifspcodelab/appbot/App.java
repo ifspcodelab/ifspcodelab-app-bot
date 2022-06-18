4374758519a3019173922ac00aa55057fbc7f4a3
Domingos Latorre <domingos.latorre@gmail.com>
2022-06-18 14:22:10 -0300

    chore: config java runtime 17 and procfile for heroku
    References:
    - system properties file: https://devcenter3.assets.heroku.com/changelog-items/2268
    - deploy gradle apps: https://devcenter.heroku.com/articles/deploying-gradle-apps-on-heroku

--- app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/App.java
+++ app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/App.java
@@ -2,13 +2,19 @@ package br.edu.ifsp.ifspcodelab.appbot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 
 public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
+
     public static void main(String[] args) throws LoginException {
         String token = System.getenv("BOT_TOKEN");
         JDA jda = JDABuilder.createDefault(token).build();
+        LOGGER.info("Bot started");
         jda.addEventListener(new AppListenerAdapter());
+        LOGGER.info("Bot configured");
     }
 }
