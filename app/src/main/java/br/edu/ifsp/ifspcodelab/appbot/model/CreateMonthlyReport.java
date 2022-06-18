fb3c70fe9fe38ac70c3dd7a12d2f53671705021b
Domingos Latorre <domingos.latorre@gmail.com>
2022-06-18 15:50:38 -0300

    refactor: use bean validation hibernate validation

---
+++ app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/model/CreateMonthlyReport.java
@@ -0,0 +1,26 @@
+package br.edu.ifsp.ifspcodelab.appbot.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class CreateMonthlyReport {
+    @NonNull
+    ParticipationType participationType;
+    @NonNull
+    LocalDate data;
+    @NonNull @NotEmpty @Size(min = 5, max = 100, message = "Nome deve ter entre 5 e 100 caracteres")
+    String name;
+    @NonNull @NotEmpty @Size(min = 100, max = 600, message = "Atividades planejadas deve ter entre 5 e 100 caracteres")
+    String planActivities;
+    @NonNull @NotEmpty @Size(min = 100, max = 600, message = "Atividades realizadas deve ter entre 5 e 100 caracteres")
+    String executedActivities;
+    @NonNull @NotEmpty @Size(min = 100, max = 600, message = "Resultados obtidos deve deve ter entre 5 e 100 caracteres")
+    String results;
+}
