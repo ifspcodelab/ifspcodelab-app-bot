11087a20f7c0a62d3a4db038354df280b9c3687b
Domingos Latorre <domingos.latorre@gmail.com>
2022-06-18 15:23:00 -0300

    feat: create report command and validate

--- app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/App.java
+++ app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/App.java
@@ -1,20 +1,18 @@
 package br.edu.ifsp.ifspcodelab.appbot;
 
+import lombok.extern.java.Log;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 
+@Log
 public class App {
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
-
     public static void main(String[] args) throws LoginException {
         String token = System.getenv("BOT_TOKEN");
         JDA jda = JDABuilder.createDefault(token).build();
-        LOGGER.info("Bot started");
+        log.info("Bot started");
         jda.addEventListener(new AppListenerAdapter());
-        LOGGER.info("Bot configured");
+        log.info("Bot configured");
     }
 }
