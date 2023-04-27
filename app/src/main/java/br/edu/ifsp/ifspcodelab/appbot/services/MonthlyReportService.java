70640bebad8d5089274b8b384362840682b556d5
rafaelspa <rsdepaula67@gmail.com>
2023-04-27 17:55:49 -0300

    feat: update monthly report

--- app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/services/MonthlyReportService.java
+++ app/src/main/java/br/edu/ifsp/ifspcodelab/appbot/services/MonthlyReportService.java
@@ -33,53 +33,37 @@ public class MonthlyReportService {
 
             // Define common fonts
             Font fontTitle = new Font(Font.HELVETICA, 13, Font.BOLD);
+            Font fontHeader = new Font(Font.HELVETICA, 10, Font.BOLD);
             Font fontTimesBold = new Font(Font.HELVETICA, Font.DEFAULTSIZE, Font.BOLD);
             Font fontDefault = new Font(Font.HELVETICA, Font.DEFAULTSIZE, Font.NORMAL);
 
-            // Page Header
-            PdfPTable pageHeader = new PdfPTable(5);
-            pageHeader.setWidthPercentage(90);
-            pageHeader.setSpacingAfter(15);
-            pageHeader.getDefaultCell().setBorderColor(new Color(255, 0, 0));
+            // New Header
             Image imageIf = Image.getInstance(getClass().getResource("/logofed_1.jpg"));
-            PdfPCell imageIfCell = new PdfPCell();
-            imageIfCell.setImage(imageIf);
-            imageIfCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            imageIfCell.setHorizontalAlignment(Element.ALIGN_LEFT);
-            imageIfCell.setFixedHeight(80);
-            imageIfCell.setBorderColor(Color.WHITE);
-            imageIfCell.setColspan(1);
-            PdfPCell textCell = new PdfPCell();
-            textCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            textCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            textCell.addElement(generatePageHeaderText("EDITAL Nº SPO.091, DE 1º DE DEZEMBRO DE 2021", fontTitle));
-            textCell.addElement(generatePageHeaderText("PROGRAMA DE PROJETOS DE ENSINO", fontTitle));
-            textCell.addElement(generatePageHeaderText("PARTICIPAÇÃO VOLUNTÁRIA", fontTitle));
-            textCell.addElement(generatePageHeaderText("CHAMADA DE PROJETOS 2022", fontTitle));
-            textCell.setBorderColor(Color.WHITE);
-            textCell.setColspan(4);
-            pageHeader.addCell(imageIfCell);
-            pageHeader.addCell(textCell);
-            document.add(pageHeader);
-
-            // Titles
-            document.add(generateTitle("ANEXO IV", fontTitle));
-            document.add(generateTitle("Relatório Mensal de Frequência e Avaliação - 2022", fontTitle));
+            imageIf.setSpacingBefore(6.0f);
+            imageIf.setAlignment(Element.ALIGN_CENTER);
+            imageIf.scalePercent(65);
+            document.add(imageIf);
+            document.add(generateTitle("MINISTÉRIO DA EDUCAÇÃO", fontHeader));
+            Paragraph p2 = generateTitle("INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DE SÃO PAULO", fontHeader);
+            p2.setSpacingAfter(6.0f);
+            document.add(p2);
+            document.add(generateTitle("EDITAL Nº SPO.009, DE 1º DE FEVEREIRO DE 2023", fontHeader));
+            document.add(generateTitle("ANEXO IV- RELATÓRIO MENSAL DE FREQUÊNCIA E AVALIAÇÃO – 2023", fontHeader));
 
             // First Table
-            PdfPTable headerTable = new PdfPTable(3);
+            PdfPTable headerTable = new PdfPTable(10);
             headerTable.setWidthPercentage(100);
             headerTable.setSpacingBefore(15);
             headerTable.setSpacingAfter(8);
             headerTable.getDefaultCell().setBorderColor(new Color(255, 0, 0));
-            headerTable.addCell(generateCell("Título do Projeto", 1, fontTimesBold));
-            headerTable.addCell(generateCell(appConfig.getProjectTitle(), 2, fontDefault));
-            headerTable.addCell(generateCell("Professor Responsável", 1, fontTimesBold));
-            headerTable.addCell(generateCell(appConfig.getCoordinatorName(), 2, fontDefault));
-            headerTable.addCell(generateCell("Voluntário", 1, fontTimesBold));
-            headerTable.addCell(generateCell(createMonthlyReport.getName(), 2, fontDefault));
-            headerTable.addCell(generateCell("Data de entrega", 1, fontTimesBold));
-            headerTable.addCell(generateCell(createMonthlyReport.getData().format(formatter), 2, fontDefault));
+            headerTable.addCell(generateCell("Título do Projeto", 4, fontTimesBold));
+            headerTable.addCell(generateCell(appConfig.getProjectTitle(), 6, fontDefault));
+            headerTable.addCell(generateCell("Professor(a) Responsável", 4, fontTimesBold));
+            headerTable.addCell(generateCell(appConfig.getCoordinatorName(), 6, fontDefault));
+            headerTable.addCell(generateCell("Voluntário(a)", 4, fontTimesBold));
+            headerTable.addCell(generateCell(createMonthlyReport.getName(), 6, fontDefault));
+            headerTable.addCell(generateCell("Data de entrega", 4, fontTimesBold));
+            headerTable.addCell(generateCell(createMonthlyReport.getData().format(formatter), 6, fontDefault));
             document.add(headerTable);
 
             // Subtitle
@@ -88,7 +72,7 @@ public class MonthlyReportService {
                 .minus(1)
                 .getDisplayName(TextStyle.FULL, new Locale("pt", "BR"));
 
-            document.add(generateTitle("Resumo das atividades desenvolvidas no mês de " + month + "/2022", fontTitle));
+            document.add(generateTitle("Resumo das atividades desenvolvidas no Mês de " + month + "/2023", fontTitle));
 
             // Content Table
             PdfPTable contentTable = new PdfPTable(1);
@@ -105,7 +89,7 @@ public class MonthlyReportService {
             document.add(contentTable);
 
             //Message
-            String message = "Observação: Entregar este relatório via plataforma Moodle até o dia 05 de cada mês, conforme previsto no edital.";
+            String message = "Observação: Entregar este relatório via plataforma Moodle até o dia 5º de cada mês, conforme previsto no edital.";
             Paragraph messageParagraph = new Paragraph(message, fontDefault);
             messageParagraph.setSpacingAfter(6);
             document.add(messageParagraph);
@@ -117,8 +101,8 @@ public class MonthlyReportService {
             signaturesTable.getDefaultCell().setBorderColor(new Color(255, 255, 255));
             signaturesTable.addCell(generateCellCenter("________________________________", 1, fontTimesBold));
             signaturesTable.addCell(generateCellCenter("________________________________", 1, fontTimesBold));
-            signaturesTable.addCell(generateCellCenter("Voluntário", 1, fontDefault));
-            signaturesTable.addCell(generateCellCenter("Professor Responsável", 1, fontDefault));
+            signaturesTable.addCell(generateCellCenter("Voluntário(a)", 1, fontDefault));
+            signaturesTable.addCell(generateCellCenter("Professor(a) Responsável", 1, fontDefault));
             document.add(signaturesTable);
             document.close();
             return baos;
